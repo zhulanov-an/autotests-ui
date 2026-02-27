025d737ace3c4e651982a26aea8059af1b30712c
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-02-27 12:41:41 +0300

    6.3 Маркировки Pytest Практика работы с pytest маркировками

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import sync_playwright, expect
+import pytest
 
 
+@pytest.mark.courses
+@pytest.mark.regression
 def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
