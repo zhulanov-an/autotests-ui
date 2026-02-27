d8b4ef2866cdeeed44a40d8d3da25f16f1762185
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-02-27 21:16:28 +0300

    6.7 Параметризация Pytest Идентификаторы

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -1,7 +1,8 @@
 import pytest
+from _pytest.fixtures import SubRequest
 
 
-@pytest.mark.parametrize("number", [1, 2, 3, -1])
+@pytest.mark.parametrize("number", [-1, 2, 3, 4])
 def test_numbers(number: int):
     assert number > 0
 
@@ -15,3 +16,38 @@ def test_several_numbers(number: int, expected: int):
 @pytest.mark.parametrize("browser", ["chromium", "webkit", "firefox"])
 def test_multiplication_of_numbers(os: str, browser: str):
     assert len(os + browser) > 0
+
+
+@pytest.fixture(params=["chromium", "webkit", "firefox"])
+def browser(request: SubRequest) -> str:
+    return request.param
+
+
+def test_open_browser(browser: str):
+    print(f"Running test on browser: {browser}")
+
+
+@pytest.mark.parametrize("user", ["Alice", "Zara"])
+class TestOperations:
+    @pytest.mark.parametrize("account", ["Credit card", "Debit card"])
+    def test_user_with_operations(self, user: str, account: str):
+        print(f"User with operations: {user}")
+
+    def test_user_without_operations(self, user: str):
+        print(f"User without operations: {user}")
+
+
+users = {
+    "+70000000011": "User with money on bank account",
+    "+70000000022": "User without money on bank account",
+    "+70000000033": "User with operations on bank account"
+}
+
+
+@pytest.mark.parametrize(
+    "phone_number",
+    users.keys(),
+    ids=lambda phone_number: f"{phone_number}: {users[phone_number]})"
+)
+def test_identifiers(phone_number: str):
+    pass
