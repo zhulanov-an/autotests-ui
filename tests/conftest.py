d204045f6b95eaf1bdc9b15260c9f6b7bcb4e6e2
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-02-27 16:46:29 +0300

    6.5 Фикстуры Pytest Работа с фикстурами на практике

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from playwright.sync_api import sync_playwright, Page
+
+
+@pytest.fixture
+def chromium_page() -> Page:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        yield browser.new_page()
+        browser.close()
---
+++ tests/test_authorization.py
@@ -0,0 +1,21 @@
+import pytest
+from playwright.sync_api import sync_playwright, expect, Page
+
+
+@pytest.mark.regression
+@pytest.mark.authorization
+def test_wrong_email_or_password_authorization(chromium_page: Page):
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill("password")
+
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
