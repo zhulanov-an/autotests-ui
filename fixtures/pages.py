019c682b597dc0ce0e1c09d882074e849f5fa81d
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-01 01:30:14 +0300

    7.2 Практикуемся в реализации PageObject Описание страницы CreateCoursePage

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
@@ -25,3 +26,8 @@ def dashboard_page(chromium_page: Page) -> DashboardPage:
 @pytest.fixture
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
