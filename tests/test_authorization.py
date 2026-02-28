c63ddb290cc67ab4e220248a4a450823d962dda1
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-02-28 18:00:59 +0300

    7.1 Знакомство с PageObject Используем LoginPage в автотестах

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import expect, Page
 
+from pages.login_page import LoginPage
+
 
 @pytest.mark.regression
 @pytest.mark.authorization
@@ -13,17 +15,8 @@ from playwright.sync_api import expect, Page
     ]
 )
 def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    login_page = LoginPage(chromium_page)
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
