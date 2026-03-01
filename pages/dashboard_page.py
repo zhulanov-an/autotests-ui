40644c176229e6d4a977445668c79ebe26ae685c
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-01 20:53:29 +0300

    7.3 Знакомство с PageComponent Создаем NavbarComponent с помощью PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,11 +1,13 @@
 from playwright.sync_api import Page, expect
 
+from components.navigation.navbar_component import NavbarComponent
 from pages.base_page import BasePage
 
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+        self.navbar = NavbarComponent(page)
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
         self.students_title = page.get_by_test_id('students-widget-title-text')
