df8ad00d5dccdaf70161f6154695f3070005e867
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-03 00:43:56 +0300

    7.4 Практикуемся в реализации PageComponent Реализация и использование компонента CourseViewComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -43,7 +43,7 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
 
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    courses_list_page.course_view.check_visible(
         index=0,
         title="Playwright",
         max_score="100",
