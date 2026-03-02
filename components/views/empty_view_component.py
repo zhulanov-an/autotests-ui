89c2f81d4d816e2db32eae12cf9fff3ce14c6d6b
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-02 15:12:32 +0300

    7.4 Практикуемся в реализации PageComponent Практикуемся в работе с Page Component

---
+++ components/views/empty_view_component.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Page, expect
+
+from components.base_component import BaseComponent
+
+
+class EmptyViewComponent(BaseComponent):
+    def __init__(self, page: Page, identifier: str):
+        super().__init__(page)
+
+        self.icon = page.get_by_test_id(f"{identifier}-preview-empty-view-icon")
+        self.title = page.get_by_test_id(f"{identifier}-preview-empty-view-title-text")
+        self.description = page.get_by_test_id(f"{identifier}-preview-empty-view-description-text")
+
+    def check_visible(self, title: str, description: str):
+        expect(self.icon).to_be_visible()
+
+        expect(self.title).to_be_visible()
+        expect(self.title).to_have_text(title)
+
+        expect(self.description).to_be_visible()
+        expect(self.description).to_have_text(description)
