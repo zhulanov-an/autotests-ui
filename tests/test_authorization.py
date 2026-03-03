9967a45600d64f92c810718251ce69eecd4130f0
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-03 23:39:45 +0300

    7.4 Практикуемся в реализации PageComponent Практикуемся в написании компонентов с использованием паттерна PageComponent 1. LoginFormComponent

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -15,6 +15,7 @@ from pages.login_page import LoginPage
 )
 def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
     login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    login_page.fill_login_form(email=email, password=password)
+    login_page.login_form.fill(email=email, password=password)
+    login_page.login_form.check_visible(email=email, password=password)
     login_page.click_login_button()
     login_page.check_visible_wrong_email_or_password_alert()
