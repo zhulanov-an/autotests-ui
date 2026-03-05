48bddfd1d173f53235e548690c85e23a1f98dd1b
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-05 23:06:27 +0300

    7.7 Наращивание тестовой базы и рефакторинг Использование тестовых классов

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -5,17 +5,18 @@ from pages.authentication.login_page import LoginPage
 
 @pytest.mark.regression
 @pytest.mark.authorization
-@pytest.mark.parametrize(
-    "email, password",
-    [
-        ("user.name@gmail.com", "password"),
-        ("user.name@gmail.com", "  "),
-        ("  ", "password")
-    ]
-)
-def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
-    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    login_page.login_form.fill(email=email, password=password)
-    login_page.login_form.check_visible(email=email, password=password)
-    login_page.click_login_button()
-    login_page.check_visible_wrong_email_or_password_alert()
+class TestAuthorization:
+    @pytest.mark.parametrize(
+        "email, password",
+        [
+            ("user.name@gmail.com", "password"),
+            ("user.name@gmail.com", "  "),
+            ("  ", "password")
+        ]
+    )
+    def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
+        login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        login_page.login_form.fill(email=email, password=password)
+        login_page.login_form.check_visible(email=email, password=password)
+        login_page.click_login_button()
+        login_page.check_visible_wrong_email_or_password_alert()
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -6,54 +6,52 @@ from pages.courses.create_course_page import CreateCoursePage
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(courses_list_page: CoursesListPage):
-    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+class TestCourses:
+    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
+        courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    courses_list_page.navbar.check_visible("username")
-    courses_list_page.sidebar.check_visible()
+        courses_list_page.navbar.check_visible("username")
+        courses_list_page.sidebar.check_visible()
 
-    courses_list_page.toolbar_view.check_visible()
-    courses_list_page.check_visible_empty_view()
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.check_visible_empty_view()
 
+    def test_create_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+        create_course_page.create_course_toolbar.check_visible(is_create_course_disabled=True)
+        create_course_page.check_visible_exercises_empty_view()
+        create_course_page.create_course_form.check_visible(title="",
+                                                            description="",
+                                                            estimated_time="",
+                                                            max_score="0",
+                                                            min_score="0")
+        create_course_page.create_course_exercise_toolbar_view.check_visible()
+        create_course_page.check_visible_exercises_empty_view()
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_form.check_visible(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_toolbar.check_visible(is_create_course_disabled=False)
+        create_course_page.create_course_toolbar.click_create_course_button()
 
-@pytest.mark.courses
-@pytest.mark.regression
-def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_course_page.create_course_toolbar.check_visible(is_create_course_disabled=True)
-    create_course_page.check_visible_exercises_empty_view()
-    create_course_page.create_course_form.check_visible(title="",
-                                                        description="",
-                                                        estimated_time="",
-                                                        max_score="0",
-                                                        min_score="0")
-    create_course_page.create_course_exercise_toolbar_view.check_visible()
-    create_course_page.check_visible_exercises_empty_view()
-    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
-    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.create_course_form.fill(
-        title="Playwright",
-        estimated_time="2 weeks",
-        description="Playwright",
-        max_score="100",
-        min_score="10"
-    )
-    create_course_page.create_course_form.check_visible(
-        title="Playwright",
-        estimated_time="2 weeks",
-        description="Playwright",
-        max_score="100",
-        min_score="10"
-    )
-    create_course_page.create_course_toolbar.check_visible(is_create_course_disabled=False)
-    create_course_page.create_course_toolbar.click_create_course_button()
-
-    courses_list_page.toolbar_view.check_visible()
-    courses_list_page.course_view.check_visible(
-        index=0,
-        title="Playwright",
-        max_score="100",
-        min_score="10",
-        estimated_time="2 weeks"
-    )
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            max_score="100",
+            min_score="10",
+            estimated_time="2 weeks"
+        )
