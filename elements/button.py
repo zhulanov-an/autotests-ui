707eb2e6f42d392612e1561db4ec7059aea1cc3c
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-05 11:06:27 +0300

    7.6 Практическое применение PageFactory в автотестах Переписываем компонент CourseViewComponent с использованием Page Factory

--- elements/button.py
+++ elements/button.py
@@ -4,10 +4,10 @@ from elements.base_element import BaseElement
 
 
 class Button(BaseElement):
-    def check_enabled(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_enabled(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_enabled()
 
-    def check_disabled(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_disabled(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_disabled()
