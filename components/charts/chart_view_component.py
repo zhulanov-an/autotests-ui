eb996ed60ac10be8e07db1baf611ee27421c7a4d
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-05 11:37:55 +0300

    7.6 Практическое применение PageFactory в автотестах Переписываем компонент CourseViewMenuComponent с использованием Page Factory

--- components/charts/chart_view_component.py
+++ components/charts/chart_view_component.py
@@ -1,16 +1,18 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.image import Image
+from elements.text import Text
 
 
 class ChartViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str, chart_type: str):
         super().__init__(page)
-        self.title = page.get_by_test_id(f"{identifier}-widget-title-text")
-        self.chart = page.get_by_test_id(f"{identifier}-{chart_type}-chart")
+        self.title = Text(page, f"{identifier}-widget-title-text", "Title")
+        self.chart = Image(page, f"{identifier}-{chart_type}-chart", "Chart")
 
     def check_visible(self, title):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text(title)
+        self.title.check_visible(text=title)
+        self.title.check_have_text(text=title)
 
-        expect(self.chart).to_be_visible()
+        self.chart.check_visible()
