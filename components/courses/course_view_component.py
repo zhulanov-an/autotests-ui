25d8b4bdf85c73f11ac67fbf6ee8af092ab05b74
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-03-05 11:17:46 +0300

    7.6 Практическое применение PageFactory в автотестах Переписываем компонент CourseViewComponent с использованием Page Factory

--- components/courses/course_view_component.py
+++ components/courses/course_view_component.py
@@ -1,7 +1,9 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
 from components.courses.course_view_menu_component import CourseViewMenuComponent
+from elements.image import Image
+from elements.text import Text
 
 
 class CourseViewComponent(BaseComponent):
@@ -10,23 +12,23 @@ class CourseViewComponent(BaseComponent):
 
         self.menu = CourseViewMenuComponent(page)
 
-        self.title = page.get_by_test_id('course-widget-title-text')
-        self.image = page.get_by_test_id('course-preview-image')
-        self.max_score_text = page.get_by_test_id('course-max-score-info-row-view-text')
-        self.min_score_text = page.get_by_test_id('course-min-score-info-row-view-text')
-        self.estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
+        self.title = Text(page, 'course-widget-title-text', 'Title')
+        self.image = Image(page, 'course-preview-image', 'Preview')
+        self.max_score_text = Text(page, 'course-max-score-info-row-view-text', 'Max score')
+        self.min_score_text = Text(page, 'course-min-score-info-row-view-text', 'Min score')
+        self.estimated_time_text = Text(page, 'course-estimated-time-info-row-view-text', 'Estimated time')
 
     def check_visible(self, index: int, title: str, max_score: str, min_score: str, estimated_time: str):
-        expect(self.image).to_be_visible()
+        self.image.check_visible(nth=index)
 
-        expect(self.title.nth(index)).to_be_visible()
-        expect(self.title.nth(index)).to_have_text(title)
+        self.title.check_visible(nth=index)
+        self.title.check_have_text(text=title, nth=index)
 
-        expect(self.max_score_text.nth(index)).to_be_visible()
-        expect(self.max_score_text.nth(index)).to_have_text(f"Max score: {max_score}")
+        self.max_score_text.check_visible(nth=index)
+        self.max_score_text.check_have_text(text=f"Max score: {max_score}", nth=index)
 
-        expect(self.min_score_text.nth(index)).to_be_visible()
-        expect(self.min_score_text.nth(index)).to_have_text(f"Min score: {min_score}")
+        self.min_score_text.check_visible(nth=index)
+        self.min_score_text.check_have_text(text=f"Min score: {min_score}", nth=index)
 
-        expect(self.estimated_time_text.nth(index)).to_be_visible()
-        expect(self.estimated_time_text.nth(index)).to_have_text(f"Estimated time: {estimated_time}")
+        self.estimated_time_text.check_visible(nth=index)
+        self.estimated_time_text.check_have_text(text=f"Estimated time: {estimated_time}", nth=index)
