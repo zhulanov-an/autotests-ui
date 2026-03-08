973f2b2c438177f6017f3db64310b5d3c98ddcea
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-08 22:08:59 +0300

    9.4 Моки и оптимизация UI автотестов Работаем с моками в Playwright

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,7 @@ import allure
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
 
 
 def initialize_playwright_page(
@@ -16,6 +17,7 @@ def initialize_playwright_page(
         storage_state=storage_state, record_video_dir=settings.videos_dir)
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
